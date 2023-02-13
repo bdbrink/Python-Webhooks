73922a800317cb80515b0ba5100cf05b8152fbc9
bdbrink <brendan.brink@outlook.com>
2023-02-13 17:31:35 -0600

    server put requests also

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# decorator for the api the local host is serving
 @app.route("/webhookcallback", methods=["POST"])
 def hook():
     print(request.data)
-    return "I'm a response"
+    return "This is a response"
+
+@app.route("/add", methods=["PUT"])
+def add():
+    print(request.data)
+    return f"This is a response and {request.data}"
 
 if __name__ == "__main__":
     app.run()
--- send_request.py
+++ send_request.py
@@ -2,7 +2,20 @@
 
 import requests
 
-url = "http://127.0.0.1:5000/webhookcallback"
+def callback():
+    # local host
+    url = "http://127.0.0.1:5000/webhookcallback"
 
-r = requests.post(url)
-print(r)
+    r = requests.post(url)
+    print(r.content)
+
+def add_response():
+
+    url = "http://127.0.0.1:5000/add"
+
+    r = requests.put(url, "I'm adding this")
+    print(r.content)
+
+if __name__ == "__main__":
+    callback()
+    add_response()
