832751c81fee01f9bb02ae4417afbed788948074
bdbrink <brendan.brink@outlook.com>
2023-02-13 17:20:12 -0600

    adding client server and request

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+# /usr/bin/env python3
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
-#@app.route("/webhookcallback")
+@app.route("/webhookcallback", methods=["POST"])
+def hook():
+    print(request.data)
+    return "I'm a response"
+
+if __name__ == "__main__":
+    app.run()
---
+++ send_request.py
@@ -0,0 +1,8 @@
+# /usr/bin/env python3
+
+import requests
+
+url = "http://127.0.0.1:5000/webhookcallback"
+
+r = requests.post(url)
+print(r)
